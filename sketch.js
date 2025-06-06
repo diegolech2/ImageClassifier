6d1a9fcf77201e85518ced955d3be78b69cea986
diegolech2 <diegolechugar@gmail.com>
2025-06-05 19:41:46 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,67 +1,139 @@
+// Classifier Variable
 let classifier;
-let img;
-let imgA;
-let label;
-let confidence;
-//copiar modelo de teachable machine en esta variable
-let ImageModelURL = "https://teachablemachine.withgoogle.com/models/mKvvNR7o9/";
+// Model URL
+let imageModelURL = "https://teachablemachine.withgoogle.com/models/mKvvNR7o9/";
 
+
+// A variable to hold the video we want to classify
+let video;
+
+// Variable for displaying the results on the canvas
+let label = "Model loading...";
+
+// Load the model first
 function preload() {
+  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
 
-    classifier = ml5.imageClassifier(ImageModelURL + "model.json");
-    img = loadImage("image/arduino_uno.jpg");
-    imgA = loadImage("image/bare_conductive.jpg");
-  }
+
+    // Load images from the "images" folder
+    iphoneImg = loadImage("data/nophone.png");
+    yetiImg = loadImage("data/yeti.jpg");
+    bookImg = loadImage("data/quiubole.jpg");
+    nothingImg = loadImage("data/detective.jpg");
+   
+
+}
 
 function setup() {
+  createCanvas(640, 480);
+  background(255);
 
-    createCanvas(800, 480);
-    background(255,0,0);
-  
-    image(img,0,0, 800, 480);
-    classifier.classify(img, gotResult);
+  // Using webcam feed as video input, hiding html element to avoid duplicate with canvas
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
 
 
-  }
-  
-  function draw() {
-    
-    textSize(20);
+  classifier.classifyStart(video, gotResult);
+}
+
+function draw() {
+  // Each video frame is painted on the canvas
+  image(video, 0, 0);
+
+  // Printing class with the highest probability on the canvas
+    // Draw the label
     fill(255);
-    stroke(0);
-    strokeWeight(4);
+    textSize(16);
+    textAlign(CENTER);
+    text(label, width / 2, height - 4);
+    print(label);
 
-    text("Presiona una tecla de la máquina de estados para cambiar de clasificación", 10,30);
-    if (keyIsPressed === true) {
-        if (key === 'w') {
-            classifier.classify(img, gotResult);
-            image(img,0,0, 800, 480);
-        } else if (key === 's') {
-            classifier.classify(imgA, gotResult);
-            image(imgA,0,0, 800, 480);
-        } else if (key === 'a') {
-            print("etiqueta C");
-        } else if (key === 'd') {
-          print("etiqueta D");
-        }
-      }
-  }
 
+  //State Machine
+  //cambiar condicional de etiqeta (label) basado en las clases de Teachable Machine para ejecutar cadena interactiva
+
+    if (label == 'iPhone') {
+
+      image(iphoneImg, 500, 100, 100, 100);    
+
+      fill(255);
+      textSize(16);
+      textAlign(CENTER);
+      text(label, width / 2, height - 4);
+      print(label);
+      print('pasaste por etiqueta1');
+
+
+  } else if(label == 'Yeti'){
+      image(video, 0, 0);
+
+      image(yetiImg, 500, 100, 100, 100);
+
+      fill(255,0,0,100);
+      rect(0,0,640,480);
+
+      fill(255);
+      textSize(16);
+      textAlign(CENTER);
+      text(label, width / 2, height - 4);
+
+      print(label);
+      print('pasaste por etiqueta2');
+
+  }else if(label == 'Book'){
+      image(video, 0, 0);
+      image(bookImg, 500, 100, 100, 100);
+      fill(0,255,0,100);
+      rect(0,0,640,480);
+
+      fill(255);
+      textSize(16);
+      textAlign(CENTER);
+      text(label, width / 2, height - 4);
+
+      print(label);
+      print('pasaste por etiqueta3');
+
+  }else if(label == 'Nothing'){
+      image(video, 0, 0);
+      image(nothingImg, 500, 100, 100, 100);
+
+      fill(0,0,255,100);
+      rect(0,0,640,480);
+
+      fill(255);
+      textSize(16);
+      textAlign(CENTER);
+      text(label, width / 2, height - 4);
+
+      print(label);
+      print('pasaste por etiqueta4');  
+      
+  }else if(label == 'Andres'){
+    image(video, 0, 0);
+
+    fill(0,0,255,100);
+    rect(0,0,640,480);
+ 
+    
+    fill(255);
+    textSize(16);
+    textAlign(CENTER);
+    text(label, width / 2, height - 4);
+
+    print(label);
+    print('pasaste por etiqueta4');  
+    
+
+}
+  
+}
 
 // Callback function for when classification has finished
 function gotResult(results) {
-    // The results are in an array ordered by confidence
-    console.log(results);
-    // Display the results on the canvas
-    fill(255,0,0);
-    stroke(0);
-    textSize(30);
-
-    label = "Label: " + results[0].label;
-    confidence = "Confidence: " + nf(results[0].confidence, 0, 2);
-    text(label, 10, 360);
-    text(confidence, 10, 400);
-  
-  }
+  // Update label variable which is displayed on the canvas
+  label = results[0].label;
+}
 
   
